93be0007fcdd794db45e64172ea8fb57a7a07f7e
Ash Muir <ashley.muir@locatrix.com>
2026-02-26 13:38:37 +1000

    ESP-212 (actually 212 this time) Code Clean Up

--- EspGisViewer/src/Routes/IndexRoute.cs
+++ EspGisViewer/src/Routes/IndexRoute.cs
@@ -13,18 +13,18 @@ namespace EspGisViewer.Routes
 
             context.Response.Write($@"<!DOCTYPE html>
 <html>
-    <head>
+  <head>
     <title>ESP GIS Server</title>
-    </head>
-    <body>
+  </head>
+  <body>
     <h1>ESP GIS Server version 0.6.0</h1>
     <h2><a href=""{ServerHost.GetServerUrl(context.Request, parameters.GetValue("accessToken"), true)}viewer"">Online Map Viewer</a></h2>
     <h2><a href=""{ServerHost.GetServerUrl(context.Request, parameters.GetValue("accessToken"), true)}wfs"">WFS Endpoint</a></h2>
     <h2><a href=""{ServerHost.GetServerUrl(context.Request, parameters.GetValue("accessToken"), true)}wmts/capabilities.xml"">WMTS Endpoint (all layers)</a></h2>
     <p>Per-layer WMTS endpoints can be accessed by visiting the Map Viewer and clicking the ""Copy WMTS URL"" button.</p>
-    </body>
+  </body>
 </html>
-            ");
+");
             return Task.CompletedTask;
         }
     }
